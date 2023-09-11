ef0839a3a3b5913bb80ed5e861292866d99794ef
imraunav <raun.ghosh@gmail.com>
2023-09-11 10:36:00 +0530

    added dictionary training

---
+++ learn_dictionary.py
@@ -0,0 +1,78 @@
+from sklearn.decomposition import DictionaryLearning
+import cv2
+import numpy as np
+import pickle
+from tqdm import tqdm 
+import os
+from sklearn.preprocessing import normalize
+import time
+
+
+from utils import read_im, process_patch, patch_pruning, bgr2ycrcb
+
+def mycallback(arg):
+    # print(type(arg))
+    # print(arg.keys())
+    print("Current cost: ", arg['current_cost'])
+    # print("Dictionary: ", type(arg['dictionary'])) #np.nparray
+    save(arg['dictionary'], './dictionaries/ckpt/ckpt.pkl')
+
+def save(D, name):
+    with open(name, mode='wb') as file:
+        pickle.dump(D, file)
+
+def main():
+    n_atoms = 1024
+    batch_size = 50
+    max_iter = 1000
+    patchsize = 3
+    up_scale = 3
+
+    print("Reading samples from /trainset/ ...")
+    path = './trainset'
+    Xh = []
+    Xl = []
+    for filename in tqdm(os.listdir(path)):
+        filepath = os.path.join(path, filename)
+        im = read_im(filepath) 
+        ycrcb = bgr2ycrcb(im)
+        y = ycrcb[:, :, 0]
+        hp, lp = process_patch(y, patchsize=patchsize, scale=3)
+        Xh.extend(hp)
+        Xl.extend(lp)
+    print("Done!")
+    Xh = np.array(Xh)
+    Xl = np.array(Xl)
+
+    Xh, Xl = patch_pruning(Xh, Xl)
+
+    print('Training dictionary...')
+    #preprocessing
+    Xh = normalize(Xh, axis=1)
+    Xl = normalize(Xl, axis=1)
+    N = Xh.shape[1]
+    M = Xl.shape[1]
+    coupled_patches = np.concatenate((Xh*np.sqrt(N), Xl*np.sqrt(M)), axis=1)
+    tic = time.perf_counter()
+    dict_learn_compact = DictionaryLearning(n_components=n_atoms, 
+                                            alpha=1, 
+                                            verbose=True,
+                                            transform_algorithm='omp', 
+                                            max_iter=max_iter,
+                                            n_jobs=-1,
+                                            callback=mycallback)
+    dict_learn_compact.fit(coupled_patches)
+    Dc = dict_learn_compact.components_
+    toc = time.perf_counter()
+    print(f'Time taken to learn the coupled dictionary = {(toc-tic)/60:.5} mins')
+
+    # decoupling
+    Dh = Dc[:, :N]
+    Dl = Dc[:, N:]
+    save(Dh, f"./dictionaries/Dh_{n_atoms}_{patchsize}_{max_iter}.pkl")
+    save(Dl, f"./dictionaries/Dl_{n_atoms}_{patchsize}_{max_iter}.pkl")
+
+    print("Dictionaries saved!")
+
+if __name__ == "__main__":
+    main()
--- utils.py
+++ utils.py
@@ -1,13 +1,104 @@
 import numpy as np
+import cv2
+from scipy.signal import convolve2d
 
-def path_pruning(Xh, Xl):
+def patch_pruning(Xh, Xl):
     # remove samples with low variance, i.e., blank spaces and so on
-    pvars = np.var(Xh, axis=0) # compute variane in all the images
+    pvars = np.var(Xh, axis=1) # compute variane in all the images
     threshold = np.percentile(pvars, 10) # compute 10-th percentile 
     idx = pvars > threshold # binary indexing
-    Xh = Xh[idx, :] # may need to change this
+    # print(Xh.shape, idx.shape)
+    Xh = Xh[idx, :] # may need to change this based on how the matrices are defined
     Xl = Xl[idx, :]
     return Xh, Xl
 
+def patchify(im, patchsize=3):
+    '''
+    Return patches of [patchsize] from images 
+    '''
+    patches = []
+    # print("Patchify", im.shape)
+    l, b = im.shape[0], im.shape[1]
+    for i in range(0, l, patchsize):
+        for j in range(0, b, patchsize):
+            patch = im[i:i+patchsize, j:j+patchsize]
+            patches.append(patch)
+    return np.array(patches)
 
+def rescale(im, scale):
+    '''
+    resize by a factor
+    '''
+    new_im = cv2.resize(im, (0,0), fx=scale, fy=scale) 
+    return new_im
+
+def resize(im, dim):
+    '''
+    resize by dim
+    '''
+    new_im = cv2.resize(im, dim)
+    return new_im
+
+def read_im(path):
+    im = cv2.imread(path, cv2.IMREAD_COLOR)
+    im = im / 255.0
+    return im.astype(np.float32)
+
+def write_im(im, path):
+    im = im * 255.0
+    im = im.round()
+    im = im.astype(np.uint8)
+    cv2.imwrite(path, im)
+
+def bgr2ycrcb(im):
+    # print(im.shape)
+    return cv2.cvtColor(im, cv2.COLOR_BGR2YCrCb)
+
+
+def lr_features(lr):
+    '''
+    computing first and second order gradients
+    '''
+    hf1 = np.array(
+        [[-1, 0, 1],
+         [-1, 0, 1],
+         [-1, 0, 1]]
+    )
+    vf1 = hf1.T
+    hf2 = np.array(
+        [[1, 0, -2, 0, 1],
+        [1, 0, -2, 0, 1],
+        [1, 0, -2, 0, 1]]
+    )
+    vf2 = hf2.T
+
+    # gradients
+    lr_g11 = convolve2d(lr, hf1, 'same')
+    lr_g12 = convolve2d(lr, vf1, 'same')
+
+    lr_g21 = convolve2d(lr, hf2, 'same')
+    lr_g22 = convolve2d(lr, vf2, 'same')
+
+    lr_f = np.empty((*lr.shape[:2], 4))
+    lr_f[:,:,0] = lr_g11
+    lr_f[:,:,1] = lr_g12
+    lr_f[:,:,2] = lr_g21
+    lr_f[:,:,3] = lr_g22
+    return lr_f
+
+
+def process_patch(hr, patchsize, scale):
+    '''
+    Process patches for a given image
+    '''
+    lr = rescale(hr, scale)
+    lr = resize(lr, hr.shape[:2]) #interpolation
+    lr_feat = lr_features(lr)
+    patchsize = 3
+    hp = patchify(hr, patchsize)
+    hp = hp.reshape((hp.shape[0], -1))
+    lp = patchify(lr_feat, patchsize)
+    lp = lp.reshape((lp.shape[0], -1))
+       
+    return hp, lp
 
